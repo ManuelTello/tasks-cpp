61f1a40f7c8fcb44a1a1c8f980fe7c848ae66159
ManuelTello <93638568+ManuelTello@users.noreply.github.com>
2022-06-22 16:41:12 -0300

    Cambios menores a la terminal app

--- task.cpp
+++ task.cpp
@@ -9,19 +9,18 @@ struct Task
     int id;
     string description;
     string name;
-    bool isalive;
+    int isalive = 0;
 };
 
-Task addNewTask(int id_, string desc_, string name_, bool isalive_); 
+void addNewTask(int id_, string desc_, string name_); 
 void printMenu (vector<string> menu); 
-void deleteTask(short id_); 
-void updateDatabase(int id_, string desc_, string name_, bool isalive_);
+void deleteTask(int id_); 
+void updateDatabase(int id_, string desc_, string name_);
 void addMenu(vector<string> messages, string mapto[]);
 
-Task payload;
 vector <Task> database = {};
 vector <string> opciones_menu = { "1- Add new task", "2- Remove task", "3- Update task", "4- List my tasks", "5- Exit" };
-vector <string> new_menu = { "Nuevo id: ","Description: ","Task title: ","Task state: " };
+vector <string> new_menu = { "Nuevo id: ","Description: ","Task title: "};
 string task_in[4];
 string user_input;
 const bool ALIVE = true;
@@ -38,15 +37,18 @@ int main()
         {
             case 1:
                 addMenu(new_menu, task_in);
-                addNewTask(stoi(task_in[0]), task_in[1], task_in[2], stoi(task_in[3]));
-                cout << endl << "New task added to the database ..." << endl;
+                addNewTask(stoi(task_in[0]), task_in[1], task_in[2]);
                 break;
             case 2:
+                deleteTask(stoi(user_input));
                 break;
             case 3:
                 break;
             case 4:
-                for()
+                for(int i = 0; i < database.size(); i++)
+                {
+                    cout << endl << "-> Tasks:" << endl << " - " << database[i].id << endl << database[i].name << endl <<  database[i].description << endl << database[i].isalive << endl;
+                }
                 break;
             case 5:
                 cout << endl << "Exiting program ...";
@@ -81,22 +83,25 @@ void addMenu (vector<string> messages, string mapto[])
     }
 }
 
-Task addNewTask (int id_, string desc_, string name_, bool isalive_)
+void addNewTask (int id_, string desc_, string name_)
 {
     Task new_task;
     new_task.id = id_;
     new_task.description = desc_;
     new_task.name = name_;
-    new_task.isalive = isalive_;
 
-    return new_task;
+    database.push_back(new_task);
+    cout << endl << "New task added to the database ..." << endl;
 }
 
 
 
 void deleteTask (int id_)
 {
-    for(short i = 0; i < database.size(); i++)
+    cout << "Chose the task to delete: ";
+    cin >> user_input;
+
+    for(int i = 0; i < database.size(); i++)
     {
         if(database[i].id == id_)
         {
